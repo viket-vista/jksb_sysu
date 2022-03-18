53c1e0f5b8b75f99b1ec6e6851d982c6c7b39c7f
马子喻 <39394138+viket-vista@users.noreply.github.com>
2022-03-18 14:48:20 +0800

    Update inform_jksb.py

--- inform_jksb.py
+++ inform_jksb.py
@@ -7,12 +7,8 @@ wxsend_key = config[0]["wxsend_key"]
 
 
 def send_result(result):
-    api = "https://sc.ftqq.com/"+str(wxsend_key)+".send"
-    data = {
-        "text": "健康申报结果"+result,
-        "desp": "如遇身体不适、或居住地址发生变化，请及时更新健康申报信息。"
-    }
-    req = requests.post(api, data=data)
+    api = "https://sctapi.ftqq.com/"+str(wxsend_key)+".send?title=健康申报结果"+result+"&desp=如遇身体不适、或居住地址发生变化，请及时更新健康申报信息。"
+    req = requests.post(api)
     if req.status_code == 200:
         print("success")
     else:
